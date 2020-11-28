befda745a300be883d710703c121335bd61d6c43
jsimonis <jsimonis@student.codam.nl>
2020-11-28 18:17:04 +0100

    equals now adds their expected values as inputs for printf

--- utils/src/tu_eq.c
+++ utils/src/tu_eq.c
@@ -14,7 +14,7 @@ void tu_eq_msize(char* message, void* got, int expected, bool free_got)
 	void* correct_size = malloc(expected);
 	if (got == NULL || MALLOC_SIZE(got) != MALLOC_SIZE(correct_size))
 	{
-		tu_ko_message_exit(message);
+		tu_ko_message_exit(message, MALLOC_SIZE(got), MALLOC_SIZE(correct_size));
 	}
 	if (free_got && got)
 		try_free(got);
@@ -43,7 +43,7 @@ void tu_eq_bool(char* message, bool got, bool expected)
 void tu_eq_str(char* message, char* got, char* expected, bool free_got, bool free_expected)
 {
 	if (got != expected && (got == NULL || expected == NULL || strcmp(got,expected) != 0)) {
-		tu_ko_message_exit(message);
+		tu_ko_message_exit(message, got, expected);
 	}
 	if (free_got && got)
 		try_free(got);
@@ -53,7 +53,7 @@ void tu_eq_str(char* message, char* got, char* expected, bool free_got, bool fre
 void tu_eq_str_msize(char* message, char* msize_message, char* got, char* expected, bool free_got, bool free_expected)
 {
 	if (got != expected && (got == NULL || expected == NULL || strcmp(got,expected) != 0)) {
-		tu_ko_message_exit(message);
+		tu_ko_message_exit(message, got, expected);
 	} else if (expected != NULL) {
 		tu_eq_msize(msize_message, got, strlen(expected + 1), false);
 	}
@@ -77,7 +77,7 @@ void tu_eq_arr(char* message, void* got, void* expected, int size, bool free_got
 void tu_eq_pt(char* message, void* got, void* expected, bool free_got, bool free_expected)
 {
 	if (got != expected) {
-		tu_ko_message_exit(message);
+		tu_ko_message_exit(message, got, expected);
 	}
 	if (free_got && got)
 		try_free(got);
